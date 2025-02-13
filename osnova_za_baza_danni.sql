babc5bc4884c5edccfbc27f9517e85a452ff7ba5
Borko <107365623+Borko1234@users.noreply.github.com>
2025-02-13 12:57:53 +0200

    Update osnova_za_baza_danni.sql

--- osnova_za_baza_danni.sql
+++ osnova_za_baza_danni.sql
@@ -1,3 +1,4 @@
+DROP SCHEMA SPORT_RESERVE CASCADE;
 -- Създаване на схема
 CREATE SCHEMA IF NOT EXISTS sport_reserve;
 SET search_path TO sport_reserve;
@@ -9,8 +10,20 @@ CREATE TABLE IF NOT EXISTS sports_halls (
     mobile VARCHAR(15) NOT NULL,            -- Телефон за връзка със спортната зала
     address TEXT NOT NULL,                  -- Адрес на спортната зала
     description TEXT,                       -- Описание на спортната зала
-    schedule TEXT,                          -- Работно време на спортната зала
     entry VARCHAR(4) NOT NULL               -- free, paid, both
+);
+--SCHEDULE TABLE
+CREATE TABLE IF NOT EXISTS schedule(
+	id SERIAL PRIMARY KEY,-- schedule's id we use in the sport_hall_schedule
+	open TIME(0) NOT NULL,
+	close TIME(0) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS sport_hall_schedule(
+	sports_hall_id INT NOT NULL,
+	schedule_id INT NOT NULL,
+	CONSTRAINT fk_sports_hall FOREIGN KEY (sports_hall_id) REFERENCES sports_halls(id)
+	CONSTRAINT fk_schedule FOREIGN KEY (schedule_id) REFERENCES schedule(id)
 );
 
 -- Таблица за потребители
@@ -28,7 +41,8 @@ CREATE TABLE IF NOT EXISTS reservations (
     id SERIAL PRIMARY KEY,                  -- Уникален идентификатор за всяка резервация
     sports_hall_id INT NOT NULL,            -- ID на спортната зала
     user_id INT NOT NULL,                   -- ID на потребителя
-    date TIMESTAMP NOT NULL,                -- Дата и час на резервацията
+    date TIMESTAMP WITHOUT TIME ZONE NOT NULL,    -- Дата и час на резервацията
+	duration INT NOT NULL,					--запазваме колко време ще е резервацията (в часове),
     description TEXT,                       -- Описание на резервацията
     CONSTRAINT fk_sports_hall FOREIGN KEY (sports_hall_id) REFERENCES sports_halls(id), -- Връзка със sports_halls
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) -- Връзка с users
@@ -74,8 +88,18 @@ CREATE TABLE IF NOT EXISTS reviews (
 
 -- Таблица за удобства на спортните зали
 CREATE TABLE IF NOT EXISTS sports_hall_facilities (
-    id SERIAL PRIMARY KEY,                  -- Уникален идентификатор за удобствата
     sports_hall_id INT NOT NULL,            -- ID на спортната зала
-    facility VARCHAR(100) NOT NULL,         -- Описание на удобството (напр. паркинг)
-    CONSTRAINT fk_facility_sports_hall FOREIGN KEY (sports_hall_id) REFERENCES sports_halls(id)
+	facility_id INT NOT NULL,				 -- facility's id
+    CONSTRAINT fk_sports_hall FOREIGN KEY (sports_hall_id) REFERENCES sports_halls(id)
+	CONSTRAINT fk_facility FOREIGN KEY (facility_id) REFERENCES facilities(id)
+);
+--FACILITIES TABLE
+CREATE TABLE IF NOT EXISTS facilities(
+	id SERIAL PRIMARY KEY,--id for each facility
+	name VARCHAR(100)--name of the facility
+);
+--SPORT_HALL_TYPES 
+CREATE TABLE IF NOT EXISTS sports_hall_types(
+	id SERIAL PRIMARY KEY,--id for each type
+	name VARCHAR(100)--type name
 );
